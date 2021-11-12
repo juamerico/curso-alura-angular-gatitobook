e348f09acd2316c9d42f3456dc9f3686d6b8f663
Juliana Americo <juu.americo@gmail.com>
2021-11-12 17:23:52 -0300

    Formulário de login criado

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
