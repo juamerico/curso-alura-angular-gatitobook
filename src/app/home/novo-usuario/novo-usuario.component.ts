8a5631ebcd0ca74879cd6104bab8fcb2e52045d7
Juliana Americo <juu.americo@gmail.com>
2021-11-14 20:07:19 -0300

    Formulário de novo usuário criado

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,4 +1,7 @@
+import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./novo-usuario.component.css']
 })
 export class NovoUsuarioComponent implements OnInit {
+  public novoUsuarioForm!: FormGroup
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private novoUsuarioService: NovoUsuarioService) { }
 
   ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      fullName: ["", [Validators.required, Validators.minLength(4)]],
+      userName: [""],
+      password: [""]
+    })
+  }
+
+  cadastrar(): void {
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario
+    console.log(JSON.stringify(novoUsuario, null, 4))
   }
 
 }
