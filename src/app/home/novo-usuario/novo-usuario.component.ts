e7225b71b0f6a2019211e4b15b149af4a179d835
Juliana Americo <juu.americo@gmail.com>
2021-11-15 19:48:38 -0300

    Alterações no método cadastrar para receber novos usuários no banco de dados

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -5,6 +5,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 import { minusculoValidator } from './minusculo.validator';
 import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -17,7 +18,8 @@ export class NovoUsuarioComponent implements OnInit {
   constructor(
       private formBuilder: FormBuilder,
       private novoUsuarioService: NovoUsuarioService,
-      private usuarioExistenteService: UsuarioExisteService
+      private usuarioExistenteService: UsuarioExisteService,
+      private router: Router
     ) {}
 
   ngOnInit(): void {
@@ -33,8 +35,14 @@ export class NovoUsuarioComponent implements OnInit {
   }
 
   cadastrar(): void {
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario
-    console.log(JSON.stringify(novoUsuario, null, 4))
+    if(this.novoUsuarioForm.valid) {
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario
+      this.novoUsuarioService.cadastraNovoUsuario(novoUsuario)
+        .subscribe(
+          () => this.router.navigate([""]),
+          (err) => console.log(err)
+        )
+    }
   }
 
 }
